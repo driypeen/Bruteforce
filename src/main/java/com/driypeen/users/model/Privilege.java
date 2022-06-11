53084078516b0d8287053d9bf31888a966a69907
driypeen <driypeen@gmail.com>
2022-06-11 22:55:36 +0400

    Add entities

---
+++ src/main/java/com/driypeen/users/model/Privilege.java
@@ -0,0 +1,23 @@
+package com.driypeen.users.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Privilege {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges", fetch = FetchType.LAZY)
+    private Set<Role> roles;
+}
