451e0c65d081bb477e2776dfb570a003abbcc6f2
driypeen <driypeen@gmail.com>
2022-06-12 16:59:07 +0400

    Add user DTO

---
+++ src/main/java/com/driypeen/Bruteforce/dto/UserDTO.java
@@ -0,0 +1,43 @@
+package com.driypeen.Bruteforce.dto;
+
+import com.driypeen.Bruteforce.exception.DTOMappingException;
+import com.driypeen.Bruteforce.model.Role;
+import com.driypeen.Bruteforce.model.User;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Data
+@JsonRootName(value = "user")
+public class UserDTO {
+    private String login;
+    private String role;
+
+    public static UserDTO mapToUserDTO(User user) {
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.login = user.getLogin();
+            userDTO.role = user.getRole().getName();
+            return userDTO;
+        } catch (Exception e) {
+            log.error("Error in mapToListUserDTO");
+            throw new DTOMappingException("Error in converted users");
+        }
+    }
+
+    public static List<UserDTO> mapToListUserDTO(List<User> users) {
+        try {
+            return users
+                    .stream()
+                    .map(UserDTO::mapToUserDTO)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            log.error("Error in mapToListUserDTO");
+            throw new DTOMappingException("Error in converted users");
+        }
+    }
+}
